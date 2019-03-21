e2d9087cb10dc3f69f157e40d05be65a141bffba
Cassinix <ninicassini@gmail.com>
2019-03-21 03:42:09 +0300

    feat: add demo version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let result = 0;
+  for (let i = 1; (5)**i < number; i++) {
+    result +=  Math.floor(number / ((5)**i));
+  }
+  console.log('result = ', result);
+  return result;
 }
